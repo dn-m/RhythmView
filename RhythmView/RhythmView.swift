c707527529aced6672fc4d7a060ed4a8a7a7bcb3
James <jbean46@gmail.com>
2017-06-14 18:06:44 +0200

    Integrate frameworks

---
+++ RhythmView/RhythmView.swift
@@ -0,0 +1,10 @@
+//
+//  RhythmView.swift
+//  RhythmView
+//
+//  Created by James Bean on 6/14/17.
+//
+//
+
+import RhythmSpellingTools
+
---
+++ RhythmViewTests/RhythmViewTests.swift
@@ -0,0 +1,15 @@
+//
+//  RhythmViewTests.swift
+//  RhythmView
+//
+//  Created by James Bean on 6/14/17.
+//
+//
+
+import XCTest
+import RhythmView
+
+class RhythmViewTests: XCTestCase {
+    
+    
+}
